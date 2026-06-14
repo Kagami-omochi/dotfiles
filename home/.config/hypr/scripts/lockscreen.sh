d151578047cccd30789953b6522baffad8006672
Kagamimochi <192094637+kagamiomochi@users.noreply.github.com>
2026-06-14 16:57:20 +0900

    Delete home/.config/hypr/scripts/lockscreen.sh

--- home/.config/hypr/scripts/lockscreen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CURRENT_WS=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .activeWorkspace.id')
-hyprctl dispatch togglespecialworkspace lock-space
-sleep 0.3
-hyprlock
-hyprctl dispatch togglespecialworkspace lock-space
-hyprctl dispatch workspace "$CURRENT_WS"
